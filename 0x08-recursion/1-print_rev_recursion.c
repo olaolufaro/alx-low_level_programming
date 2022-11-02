1904968a6ffe70235e3669ce835cab1c3aa60568
olaolufaro <farotimiolaolu@gmail.com>
2022-11-02 06:02:33 -0700

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -4,13 +4,12 @@
  *   * @s: string
  *    * Return: no return.
  */
+
 void _puts_recursion(char *s)
 {
-	if (*s != '\0')
-	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
-	}
-	else
-		_putchar('\n');
+	if (*s == '\0')
+		return;
+
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
 }
